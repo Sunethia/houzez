5f862c61a443e6073f17cbea28dc4b281fc816ac
Sunethia <lawrencesunethia@gmail.com>
2022-07-18 20:32:23 +0200

    changes

--- admin.js
+++ admin.js
@@ -1,232 +1,273 @@
-let listings = JSON.parse(localStorage.getItem("listings"))
-  ? JSON.parse(localStorage.getItem("listings"))
-  : [
-      {
-        img: "./images/apartment1.jpg",
-        title: "Renovated studio",
-        type: "double",
-        location: "194 Mercer Street,627 Broadway,New york,NY 10012, USA",
-        price: "R500",
-        card_icon: 3,
-        card_icon1: 2,
-        card_icon2: 2,
-        card_icon3: "1200 Sq M",
-        bot_head: "Apartment",
-      },
-      {
-        img: "./images/apartment2.jpg",
-        title: "Apartment with Nice View",
-        type: "double",
-        location: " 8100 Death land,Chicago,9100, USA",
-        price: "R1500",
-        card_icon: 4,
-        card_icon1: 3,
-        card_icon2: 2,
-        card_icon3: "2200 Sq M",
-        bot_head: "Apartment",
-      },
-      {
-        img: "./images/apartment3.jpg",
-        title: "Light and modern apartment ",
-        type: "double",
-        location: " 345 Sunethia street, Lansdowne, 9100, SA",
-        price: "R1800",
-        card_icon: 6,
-        card_icon1: 4,
-        card_icon2: 3,
-        card_icon3: "5000 Sq M",
-        bot_head: "Presidental Suite",
-      },
-      {
-        img: "./images/apartment4.jpg",
-        title: "Design apartment",
-        type: "double",
-        location: "Quincy street, Brooklyn, NY, USA",
-        price: "R2500",
-        card_icon: 25,
-        card_icon1: 30,
-        card_icon2: 50,
-        card_icon3: "10000 Sq M",
-        bot_head: "Palace",
-      },
-      {
-        img: "./images/apartment8.jpg",
-        title: "Fancy Apartment",
-        type: "double",
-        location: "165 Constable road, brooklyn, USA, NY",
-        price: "R2000",
-        card_icon: 94,
-        card_icon1: 100,
-        card_icon2: 150,
-        card_icon3: "50000 Sq M",
-        bot_head: "Hotel",
-      },
-      {
-        img: "./images/apartment6.jpg",
-        title: "Lovely Spacious Apartment",
-        type: "double",
-        location: " 67 Main street, Chicago, NY, USA",
-        price: "R800",
-        card_icon: 4,
-        card_icon1: 3,
-        card_icon2: 2,
-        card_icon3: "1500 Sq M",
-        bot_head: "Apartment",
-      },
-      {
-        img: "./images/apartment7.jpg",
-        title: "Beautiful Apartment",
-        type: "double",
-        location: " 786 Street, Boston, Cape town",
-        price: "R1900",
-        card_icon: 4,
-        card_icon1: 3,
-        card_icon2: 2,
-        card_icon3: "1500 Sq M",
-        bot_head: "Apartment",
-      },
-      {
-        img: "./images/apartment8.jpg",
-        title: "Lovely Spacious Apartment",
-        type: "double",
-        location: " 67 Main street, Chicago, NY, USA",
-        price: "R1500",
-        card_icon: 4,
-        card_icon1: 3,
-        card_icon2: 2,
-        card_icon3: "1500 Sq M",
-        bot_head: "Apartment",
-      },
-      {
-        img: "./images/apartment9.jpg",
-        title: "Lovely Spacious Apartment",
-        type: "double",
-        location: " 67 Main street, Chicago, NY, USA",
-        price: "R1200",
-        card_icon: 4,
-        card_icon1: 3,
-        card_icon2: 2,
-        card_icon3: "1500 Sq M",
-        bot_head: "Apartment",
-      },
-      {
-        img: "./images/apartment1.jpg",
-        title: "Lovely Spacious Apartment",
-        type: "double",
-        location: " 67 Main street, Chicago, NY, USA",
-        price: "R3000",
-        card_icon: 4,
-        card_icon1: 3,
-        card_icon2: 2,
-        card_icon3: "1500 Sq M",
-        bot_head: "Apartment",
-      },
-    ];
-console.log(listings);
-// let asc = true;
-const listingsContainer = document.querySelector("#listings");
-function showlistings(listings) {
-  listingsContainer.innerHTML = "";
-  listings.forEach((listings) => {
-    listingsContainer.innerHTML += `
-    <tr>
-    <td>${listings.img}<td>
-    <td>${listings.title}<td>
-    <td>${listings.type}<td>
-    <td>${listings.carhead}<td>
-    <td>${listings.bot_head}<td>
-    <td><button
-    type="button"
-    class="btn btn-danger"
-    data-bs-toggle="modal"
-    data-bs-target="#edit${i}"
-  >
-    Edit
-  </button>
-    <i onclick="deleteItem(${listings.id})" class="fa-solid fa-trash-can p-3"></i></td>
-    <tr>
-    <div
-  class="modal fade"
-  id="edit${id}"
-  tabindex="-1"
-  aria-labelledby="exampleModalLabel"
-  aria-hidden="true"
->
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="">Edit Product</h5>
-        <button
-          type="button"
-          class="btn-close"
-          data-bs-dismiss="modal"
-          aria-label="Close"
-        ></button>
-      </div>
-      <div class="modal-body">
-        <input type="text" placeholder="Enter image" id="image-${i}" value="${listings.img}"/>
-        <input type="text" placeholder="Enter title" id="title-${i}" value="${listings.title}" />
-        <input type="text" placeholder="Enter type" id="type-${i}" value="${listings.type}"/>
-        <input type="text" placeholder="Enter carhead" id="carhead-${i}" value="${listings.carhead}"/>
-        <input type="text" placeholder="Enter bot_head" id="bot_head-${i}" value="${listings.bot_head}"/>
-      </div>
-      <div class="modal-footer">
-        <button
-          type="button"
-          class="btn btn-secondary"
-          data-bs-dismiss="modal"
-        >
-          Cancel
-        </button>
-        <button
-        data-bs-toggle="modal"
-        data-bs-target="#edit-${i}"
-          type="button"
-          class="btn btn-primary"
-          onclick="editProperty(${i})"
-        >
-          Save changes
-        </button>
-      </div>
-    </div>
-  </div>
-</div>
-       `;
+let houses = JSON.parse(localStorage.getItem("houses"))
+? JSON.parse(localStorage.getItem("houses")):[
+    {id:1,
+        image: 'https://picsum.photos/id/1008/200/300',
+     title:'Light and Modern Apartment',
+     price: '4500',
+     address:'2436 SW 8th St, Los Angeles, FL 33135, USA',
+     bedrooms:'3',
+     bathrooms:'2',
+     garage:'1',
+     size:'1200',
+     type:'Apartment',
+     user:' Samuel Palmer',
+     datePosted:'04/04/2020',
+     yearBuilt:'2016',
+    },
+
+    {id:2,
+        image:'https://picsum.photos/id/164/200/300',
+    title:'Cute Apartment',
+    price: '4500',
+    address:'2436 SW 8th St, Los Angeles, FL 33135, USA',
+    bedrooms:'3',
+    bathrooms:'2',
+    garage:'1',
+    size:'1200',
+    type:'Apartment',
+    user:' Samuel Palmer',
+    datePosted:'04/04/2020',
+    yearBuilt:'2016',
+   },
+   {id:3,
+    image: 'https://picsum.photos/id/1062/200/300',
+   title:'Cozy Apartment',
+   price: '4500',
+   address:'2436 SW 8th St, Miami, FL 33135, USA',
+   bedrooms:'2',
+   bathrooms:'2',
+   garage:'1',
+   size:'1200',
+   type:'Apartment',
+   user:' Samuel Palmer',
+   datePosted:'04/04/2020',
+   yearBuilt:'2016',
+  },
+  {id:4,
+    image: 'https://picsum.photos/id/238/200/300',
+  title:'Newly Built Apartment',
+  price: '4500',
+  address:'2436 SW 8th St, Chicago, FL 33135, USA',
+  bedrooms:'3',
+  bathrooms:'2',
+  garage:'1',
+  size:'1200',
+  type:'Apartment',
+  user:' Samuel Palmer',
+  datePosted:'04/04/2020',
+  yearBuilt:'2016',
+ },
+ {id:5,
+    image: 'https://picsum.photos/id/221/200/300',
+ title:'Fully Furnished Apartment',
+ price: '6000',
+ address:'2436 SW 8th St, Los Angeles, FL 33135, USA',
+ bedrooms:'2',
+ bathrooms:'4',
+ garage:'1',
+ size:'1200',
+ type:'Apartment',
+ user:' Samuel Palmer',
+ datePosted:'04/04/2020',
+ yearBuilt:'2016',
+},
+{id:6,
+    image: 'https://picsum.photos/id/556/200/300',
+     title:'North Facing House',
+     price: '6000',
+     address:'2436 SW 8th St, Miami, FL 33135, USA',
+     bedrooms:'5',
+     bathrooms:'2',
+     garage:'1',
+     size:'1200',
+     type:'House',
+     user:' Samuel Palmer',
+     datePosted:'04/04/2020',
+     yearBuilt:'2016',
+    },
+    {id:7,
+        image: 'https://picsum.photos/id/594/200/300',
+    title:'Newly Built House',
+    price: '4500',
+    address:'2436 SW 8th St, New York, FL 33135, USA',
+    bedrooms:'4',
+    bathrooms:'2',
+    garage:'1',
+    size:'1200',
+    type:'House',
+    user:' Samuel Palmer',
+    datePosted:'04/04/2020',
+    yearBuilt:'2016',
+   },
+   {id:8,
+    image: 'https://picsum.photos/id/142/200/300',
+   title:'Grand Mansion',
+   price: '10000',
+   address:'2436 SW 8th St, Miami, FL 33135, USA',
+   bedrooms:'5',
+   bathrooms:'2',
+   garage:'1',
+   size:'1200',
+   type:'House',
+   user:' Samuel Palmer',
+   datePosted:'04/04/2020',
+   yearBuilt:'2016',
+  },
+  {id:9,
+    image: 'https://picsum.photos/id/604/200/300',
+  title:'Bachelor Pad',
+  price: '4500',
+  address:'2436 SW 8th St, Chicago, FL 33135, USA',
+  bedrooms:'1',
+  bathrooms:'2',
+  garage:'1',
+  size:'1200',
+  type:'Studio',
+  user:' Samuel Palmer',
+  datePosted:'04/04/2020',
+  yearBuilt:'2016',
+ },
+ {id:10,
+    image: 'https://picsum.photos/id/1031/200/300',
+ title:'Penthouse Open Floor',
+ price: '10000',
+ address:"'2436 SW 8th St',' New York','FL 33135 USA'",
+ bedrooms:'2',
+ bathrooms:'2',
+ garage:'1',
+ size:'1200',
+ type:'Studio',
+ user:' Samuel Palmer',
+ datePosted:'04/04/2020',
+ yearBuilt:'2016',
+},];
+
+localStorage.setItem("houses", JSON.stringify(houses));
+// display local storage    
+// document.getElementById("propertyList").innerHTML = JSON.parse( localStorage.getItem("houses"));
+    const propertiesTable = document.querySelector("#propertylist");
+
+function ShowHouseListings (houses) {
+    document.querySelector("#propertylist").innerHTML = "";
+  
+    houses.forEach(house => {
+      propertiesTable.innerHTML += `
+        <tr>
+        <td>${house.id}</td>
+          <td><img src=${house.image}></td>
+          <td>${house.title}</td>
+          <td>${house.address}</td>
+          <td>${house.type}</td>
+          <td>${house.bedrooms}</td>
+          <td>${house.bathrooms}</td>
+          <td>${house.garage}</td>
+          <td>${house.size}sq ft</td>
+          <td>${house.price}</td>
+          <td><button onclick="editHouse(${house.id})">✎</button> </td><td><button onclick="deleteHouse(${house.id})">✂</button></td>
+        <tr>
+      `;
+    })
+  };
+  
+ShowHouseListings(houses);
+
+// delete function
+function deleteHouse(id) {
+  houses = houses.filter((house) => {
+    return house.id !== id;     //loop through houses and return houses which id noes not match
+  });   
+  localStorage.setItem("houses", JSON.stringify(houses));
+  ShowHouseListings(houses);
+}
+
+
+// edit house listing 
+function editHouse(id) {
+  let house = houses.find((house) => house.id === id); //Finds the id of button that was clicked
+  let input = prompt("Input Changes"); //Allows the user to input their own changes
+  house.title = input; //Changes the content to what the user's input
+  // addToStorage(objectives); //Adds to local storage
+  ShowHouseListings(houses);
+}
+
+function addListing() {
+  let listing = {
+    id: houses.length + 1,
+    title: document.querySelector("#Title").value,
+    area: document.querySelector("#Location").value,
+    image: document.querySelector("#Image").value,
+    type: document.querySelector("#Type").value,
+    bedrooms: document.querySelector("#Bedrooms").value,
+    baths: document.querySelector("#Bathrooms").value,
+    garage: document.querySelector("#Garage").value,
+    size: document.querySelector("#Size").value,
+    price: document.querySelector("#Price").value,
+    status: document.querySelector("#Status").value,
+  };
+  houses.push(listing);
+  localStorage.setItem("properties", JSON.stringify(houses));
+  
+  ShowHouseListings(houses);
+  console.log(houses);
+}
+
+function typeHouse() {
+  let value = document.getElementById("comboA").value;
+  //display entire array when all is selected
+  if (value === "All") {
+    return ShowHouseListings(houses);
+  }
+  //create array to show only desired housesS
+  let filteredHouses = houses.filter((house) => {
+    return house.type === value     //loop through houses and return houses which id noes not match
+  });   
+  ShowHouseListings(filteredHouses);
+}
+
+function addressHouse() {
+  let value = document.getElementById("comboB").value;
+  //display entire array when all is selected
+  if (value === "All") {
+    return ShowHouseListings(houses);
+  }
+  let filteredHouses = houses.filter((house) => {
+    // create condition to search for value inside of the addrress
+    if(house.address.includes(value)){ 
+    return house.address } 
+        //loop through houses and return houses which id noes not match
+  });   
+  ShowHouseListings(filteredHouses);}
+
+function bedroomsHouse() {
+  let value = document.getElementById("comboC").value;
+  //display entire array when all is selected
+  if (value === "All") {
+    return ShowHouseListings(houses);
+  }
+  //create array to show only desired housesS
+  let filteredHouses = houses.filter((house) => {
+    return house.bedrooms === value     //loop through houses and return houses which id noes not match
+  });   
+  ShowHouseListings(filteredHouses);}
+
+function priceHouse() {
+  let value = document.getElementById("comboD").value;
+  //display entire array when all is selected
+  if (value === "All") {
+    return ShowHouseListings(houses);
+  }
+  //create array to show only desired houses
+  let filteredHouses = houses.filter((house) => {
+    return house.price === value     //loop through houses and return houses which id noes not match
+  });   
+  ShowHouseListings(filteredHouses);}
+
+function sortHouses(){
+  let price = houses.sort((a, b) => {
+    return a.price - b.price;
   });
-}
-showlistings(listings);
-function addProperty() {
-  const newPropety = {
-    img: document.querySelector("#image").value,
-    title: document.querySelector("#title").value,
-    type: document.querySelector("#type").value,
-    carhead: document.querySelector("#carhead").value,
-    bot_head: document.querySelector("#bot_head").value,
-    id: listings.length + 1,
-  };
-  listings.push(newProperty);
-  localStorage.setItem("listings", JSON.stringify(listings));
-  showlistings(listings);
-}
-document.querySelector("#addNewItem").addEventListener("click", addProperty);
-function deleteItem(id) {
-  listings = listings.filter((listings) => listings.id !== id);
-  localStorage.setItem("listings", JSON.stringify(listings));
-  showlistings(listings);
-}
-function editItems(i) {
-  let img = document.querySelector(`#image-${i}`).value;
-  let title = document.querySelector(`#title-${i}`).value;
-  let type = document.querySelector(`#type-${i}`).value;
-  let carhead = document.querySelector(`#carhead-${i}`).value;
-  let bot_head = document.querySelector(`#bot_head-${i}`).value;
-  listings[i] = {
-    img,
-    title,
-    type,
-    carhead,
-    bot_head,
-  };
-  localStorage.setItem("listings", JSON.stringify(listings));
-  readData(listings);
+  let retrievedList = price;
+  document.querySelector("#properties").innerHTML = "";
+  retrievedList.forEach((propperty) => {
+    ShowHouseListings(retrievedList);})
 }
--- index.js
+++ index.js
@@ -1,182 +1,242 @@
-let listing = JSON.parse(localStorage.getItem("listing"))
-  ? JSON.parse(localStorage.getItem("listing"))
-  : [
-      {
-        img: "./images/apartment1.jpg",
-        title: "Renovated studio",
-        type: "double",
-        location: "194 Mercer Street,627 Broadway,New york,NY 10012, USA",
-        price: "R500",
-        card_icon: 3,
-        card_icon1: 2,
-        card_icon2: 2,
-        card_icon3: "1200 Sq M",
-        bot_head: "Apartment",
-      },
-      {
-        img: "./images/apartment2.jpg",
-        title: "Apartment with Nice View",
-        type: "double",
-        location: " 8100 Death land,Chicago,9100, USA",
-        price: "R1500",
-        card_icon: 4,
-        card_icon1: 3,
-        card_icon2: 2,
-        card_icon3: "2200 Sq M",
-        bot_head: "Apartment",
-      },
-      {
-        img: "./images/apartment3.jpg",
-        title: "Light and modern apartment ",
-        type: "double",
-        location: " 345 Sunethia street, Lansdowne, 9100, SA",
-        price: "R1800",
-        card_icon: 6,
-        card_icon1: 4,
-        card_icon2: 3,
-        card_icon3: "5000 Sq M",
-        bot_head: "Presidental Suite",
-      },
-      {
-        img: "./images/apartment4.jpg",
-        title: "Design apartment",
-        type: "double",
-        location: "Quincy street, Brooklyn, NY, USA",
-        price: "R2500",
-        card_icon: 25,
-        card_icon1: 30,
-        card_icon2: 50,
-        card_icon3: "10000 Sq M",
-        bot_head: "Palace",
-      },
-      {
-        img: "./images/apartment8.jpg",
-        title: "Fancy Apartment",
-        type: "double",
-        location: "165 Constable road, brooklyn, USA, NY",
-        price: "R2000",
-        card_icon: 94,
-        card_icon1: 100,
-        card_icon2: 150,
-        card_icon3: "50000 Sq M",
-        bot_head: "Hotel",
-      },
-      {
-        img: "./images/apartment6.jpg",
-        title: "Lovely Spacious Apartment",
-        type: "double",
-        location: " 67 Main street, Chicago, NY, USA",
-        price: "R800",
-        card_icon: 4,
-        card_icon1: 3,
-        card_icon2: 2,
-        card_icon3: "1500 Sq M",
-        bot_head: "Apartment",
-      },
-      {
-        img: "./images/apartment7.jpg",
-        title: "Beautiful Apartment",
-        type: "double",
-        location: " 786 Street, Boston, Cape town",
-        price: "R1900",
-        card_icon: 4,
-        card_icon1: 3,
-        card_icon2: 2,
-        card_icon3: "1500 Sq M",
-        bot_head: "Apartment",
-      },
-      {
-        img: "./images/apartment8.jpg",
-        title: "Lovely Spacious Apartment",
-        type: "double",
-        location: " 67 Main street, Chicago, NY, USA",
-        price: "R1500",
-        card_icon: 4,
-        card_icon1: 3,
-        card_icon2: 2,
-        card_icon3: "1500 Sq M",
-        bot_head: "Apartment",
-      },
-      {
-        img: "./images/apartment9.jpg",
-        title: "Lovely Spacious Apartment",
-        type: "double",
-        location: " 67 Main street, Chicago, NY, USA",
-        price: "R1200",
-        card_icon: 4,
-        card_icon1: 3,
-        card_icon2: 2,
-        card_icon3: "1500 Sq M",
-        bot_head: "Apartment",
-      },
-      {
-        img: "./images/apartment1.jpg",
-        title: "Lovely Spacious Apartment",
-        type: "double",
-        location: " 67 Main street, Chicago, NY, USA",
-        price: "R3000",
-        card_icon: 4,
-        card_icon1: 3,
-        card_icon2: 2,
-        card_icon3: "1500 Sq M",
-        bot_head: "Apartment",
-      },
-    ];
+let houses = JSON.parse(localStorage.getItem("houses"))
+? JSON.parse(localStorage.getItem("houses")):[
+    {id:1,
+        image: 'https://picsum.photos/id/1008/200/300',
+     title:'Beautiful apartment',
+     price: '4500',
+     address:'314 South street, New York,3789',
+     bedrooms:'3',
+     bathrooms:'2',
+     garage:'1',
+     size:'1200',
+     type:'Apartment',
+     user:' Nugatto Louw',
+     datePosted:'05/09/2021',
+     yearBuilt:'2018',
+    },
 
-function readData(listing) {
-  document.querySelector("#listing").innerHTML = "";
-  listing.forEach((listing, i) => {
-    document.querySelector("#listing").innerHTML += `
-      <div class="card" style="width: 30%">
-      <img src=${listing.img} class="card-img-top img-fluid" style="max-height: 200px" />
-      <div class="card-body">
-      <p class="card-text">
-      <h4 class="card-title">${listing.title}</h4>
-      <h4>${listing.price}</h4>
-          <h6>${listing.location}</h6>
-          <i class="fa-solid fa-bed card_icon"></i>${listing.card_icon}
-          <i class="fa-solid fa-shower card_icon"></i>${listing.card_icon1}
-          <i class="fa-solid fa-car card_icon"></i>${listing.card_icon2}
-          <i class="fa-solid fa-ruler card_icon"></i>${listing.card_icon3}
-            <h6>${listing.bot_head}</h6>
-        </p>
-       </div>
-      </div>
-          `;
-  });
-}
-readData(listing);
+    {id:2,
+        image:'https://picsum.photos/id/164/200/300',
+    title:'Cozy Apartment',
+    price: '3500',
+    address:'852 Left street, USA, 5861',
+    bedrooms:'3',
+    bathrooms:'2',
+    garage:'1',
+    size:'1200',
+    type:'Apartment',
+    user:' Nugatto Louw',
+    datePosted:'05/04/2020',
+    yearBuilt:'2017',
+   },
+   {id:3,
+    image: 'https://picsum.photos/id/1062/200/300',
+   title:'Warm Apartment',
+   price: '5500',
+   address:'586 West St, Miami, FL 33135, USA',
+   bedrooms:'2',
+   bathrooms:'2',
+   garage:'1',
+   size:'1200',
+   type:'Apartment',
+   user:' Nugatto Louw',
+   datePosted:'09/05/2022',
+   yearBuilt:'2018',
+  },
+  {id:4,
+    image: 'https://picsum.photos/id/238/200/300',
+  title:'Fresh AiryApartment',
+  price: '3500',
+  address:'92 North street, Chicago USA',
+  bedrooms:'3',
+  bathrooms:'2',
+  garage:'1',
+  size:'1200',
+  type:'Apartment',
+  user:' Samuel Palmer',
+  datePosted:'04/04/2020',
+  yearBuilt:'2016',
+ },
+ {id:5,
+    image: 'https://picsum.photos/id/221/200/300',
+ title:'Fully Furnished Apartment',
+ price: '6000',
+ address:'2436 SW 8th St, Los Angeles, FL 33135, USA',
+ bedrooms:'2',
+ bathrooms:'4',
+ garage:'1',
+ size:'1200',
+ type:'Apartment',
+ user:' Nugatto Louwr',
+ datePosted:'04/04/2020',
+ yearBuilt:'2016',
+},
+{id:6,
+    image: 'https://picsum.photos/id/556/200/300',
+     title:'Fancy House',
+     price: '6000',
+     address:'2436 SW 8th St, Miami, FL 33135, USA',
+     bedrooms:'5',
+     bathrooms:'2',
+     garage:'1',
+     size:'1200',
+     type:'House',
+     user:' Nugatto Louw',
+     datePosted:'/04/2020',
+     yearBuilt:'2016',
+    },
+    {id:7,
+        image: 'https://picsum.photos/id/594/200/300',
+    title:'Newly Built House',
+    price: '4500',
+    address:'2436 SW 8th St, New York, FL 33135, USA',
+    bedrooms:'4',
+    bathrooms:'2',
+    garage:'1',
+    size:'1200',
+    type:'House',
+    user:' Samuel Palmer',
+    datePosted:'04/04/2020',
+    yearBuilt:'2016',
+   },
+   {id:8,
+    image: 'https://picsum.photos/id/142/200/300',
+   title:'Grand Mansion',
+   price: '10000',
+   address:'2436 SW 8th St, Miami, FL 33135, USA',
+   bedrooms:'5',
+   bathrooms:'2',
+   garage:'1',
+   size:'1200',
+   type:'House',
+   user:' Samuel Palmer',
+   datePosted:'02/04/2020',
+   yearBuilt:'2017',
+  },
+  {id:9,
+    image: 'https://picsum.photos/id/604/200/300',
+  title:'Bachelor Pad',
+  price: '4500',
+  address:'2436 SW 8th St, Chicago, FL 33135, USA',
+  bedrooms:'1',
+  bathrooms:'2',
+  garage:'1',
+  size:'1200',
+  type:'Studio',
+  user:' Samuel Palmer',
+  datePosted:'04/04/2020',
+  yearBuilt:'2016',
+ },
+ {id:10,
+    image: 'https://picsum.photos/id/1031/200/300',
+ title:'Penthouse Open plan',
+ price: '10000',
+ address:"'2436 SW 8th St',' New York','FL 33135 USA'",
+ bedrooms:'2',
+ bathrooms:'2',
+ garage:'1',
+ size:'1200',
+ type:'Studio',
+ user:' Samuel Palmer',
+ datePosted:'04/04/2020',
+ yearBuilt:'2016',
+},];
 
-filter = () => {
-  let FilterOption = document.querySelector("#type").value;
-  if (FilterOption !== "all") {
-    console.log(FilterOption);
-    document.querySelector("#listings").innerHTML = "";
-    let specificView = listings.filter(
-      (listing) => listing.type == FilterOption
-    );
-    specificView.forEach((listing) => {
-      document.querySelector(
-        "#listings"
-      ).innerHTML += ` <div class="card" style="width: 30%">
-      <img src=${listing.img} class="card-img-top img-fluid" style="max-height: 200px" />
-      <div class="card-body">
-      <p class="card-text">
-      <h4 class="card-title">${listing.title}</h4>
-      <h4>${listing.price}</h4>
-          <h6>${listing.location}</h6>
-          <i class="fa-solid fa-bed card_icon"></i>${listing.card_icon}
-          <i class="fa-solid fa-shower card_icon"></i>${listing.card_icon1}
-          <i class="fa-solid fa-car card_icon"></i>${listing.card_icon2}
-          <i class="fa-solid fa-ruler card_icon"></i>${listing.card_icon3}
-            <h6>${listing.bot_head}</h6>
-        </p>
-       </div>
-      </div>
-            `;
-      console.log(specificView);
-    });
-  } else {
-    display();
-  }
+   const propertiesContainer = document.querySelector("#properties");
+
+// add array to local storage   
+   localStorage.setItem("houses", JSON.stringify(houses));
+   
+// display local storage    
+document.getElementById("properties").innerHTML = JSON.parse( localStorage.getItem("houses"));
+
+function showHouses (houses) {
+    propertiesContainer.innerHTML = "";
+    houses.forEach(house => {
+        propertiesContainer.innerHTML += `
+            <div id='housecards'>
+            <span id="heart" class="heart"><i class="fas fa-heart"></i></span>
+                <img src=${ house.image } />
+                 <h3>${house.title}</h3>
+                 <p>${house.address}</p>
+                 <ul id=house-values>
+        
+                 <li><i class="fa-solid fa-bed"></i>${house.bedrooms}</li>
+                 <li><i class="fa-solid fa-shower"></i>${house.bathrooms}</li>
+                 <li><i class="fa-solid fa-dollar"></i>${house.price}/mo</li>
+                <li><i class="fa-solid fa-car"></i>${house.garage}</li>
+                <li><i class="fa-solid fa-ruler"></i>${house.size}</li>
+                
+
+
+            </div>
+        `
+    })
 };
+
+showHouses(houses);
+
+function typeHouse() {
+    let value = document.getElementById("comboA").value;
+    //display entire array when all is selected
+    if (value === "All") {
+      return showHouses(houses);
+    }
+    //create array to show only desired housesS
+    let filteredHouses = houses.filter((house) => {
+      return house.type === value     //loop through houses and return houses which id noes not match
+    });   
+    showHouses(filteredHouses);
+  }
+
+  function addressHouse() {
+    let value = document.getElementById("comboB").value;
+    //display entire array when all is selected
+    if (value === "All") {
+      return showHouses(houses);
+    }
+    let filteredHouses = houses.filter((house) => {
+      // create condition to search for value inside of the addrress
+      if(house.address.includes(value)){ 
+      return house.address } 
+          //loop through houses and return houses which id noes not match
+    });   
+    showHouses(filteredHouses);
+  }
+
+  function bedroomsHouse() {
+    let value = document.getElementById("comboC").value;
+    //display entire array when all is selected
+    if (value === "All") {
+      return showHouses(houses);
+    }
+    //create array to show only desired housesS
+    let filteredHouses = houses.filter((house) => {
+      return house.bedrooms === value     //loop through houses and return houses which id noes not match
+    });   
+    showHouses(filteredHouses);
+  }
+
+  function priceHouse() {
+    let value = document.getElementById("comboD").value;
+    //display entire array when all is selected
+    if (value === "All") {
+      return showHouses(houses);
+    }
+    //create array to show only desired houses
+    let filteredHouses = houses.filter((house) => {
+      return house.price === value     //loop through houses and return houses which id noes not match
+    });   
+    showHouses(filteredHouses);
+  }
+
+function sortHouses(){
+    let price = houses.sort((a, b) => {
+      return a.price - b.price;
+    });
+    let retrievedList = price;
+    document.querySelector("#properties").innerHTML = "";
+    retrievedList.forEach((propperty) => {
+    showHouses(retrievedList)
+  })
+}
